caf93a138b484cf4989012aae8b1939b39eaacc9
cdp1001 <cdp1001@gmail.com>
2014-08-24 17:30:29 -0700

    gdc_GamePiece class, sound system, and lots of sound effects

--- game.js
+++ game.js
@@ -4,17 +4,81 @@ var Game = {
     gamecontainer: null,
     runtime: 0,
     dx: {w: 495, h: 600},
+    sound: {
+        currentlyPlayingSong: undefined,
+        songVolume: 0.5,
+        sfxVolume: 0.5,
+        songMuted: false,
+        sfxMuted: false,
+        playSFX: function (sfx, indvSFXVolume) {
+            if (typeof volume === "undefined") {
+                volume = 1;
+            }
+            var out;
+            if (Game.sound.sfxMuted === false) {
+                out = createjs.Sound.play(sfx, {volume: indvSFXVolume * Game.sound.sfxVolume});
+            }
+            return out;
+        },
+        playSong: function (Song, FadeIn) {
+            if (Game.sound.currentlyPlayingSong) {
+                if (FadeIn) {
+                    createjs.Tween.get(Game.sound.currentlyPlayingSong).to({volume: 0}, 500).call(function (song) {
+                        song.stop();
+                    }, [Game.sound.currentlyPlayingSong]);
+                } else {
+                    Game.sound.currentlyPlayingSong.stop();
+                }
+            }
+
+            Game.sound.currentlyPlayingSong = createjs.Sound.play(Song, {loop: -1});
+
+            if (FadeIn) {
+                Game.sound.currentlyPlayingSong.volume = 0;
+                createjs.Tween.get(Game.sound.currentlyPlayingSong).to({volume: Game.sound.songVolume}, 500);
+            } else {
+                Game.sound.currentlyPlayingSong.volume = Game.sound.songVolume;
+            }
+        },
+        muteSong: function () {
+            if (Game.sound.currentlyPlayingSong) {
+                if (Game.sound.songMuted === true) {
+                    Game.sound.songMuted = false;
+                    Game.sound.currentlyPlayingSong.volume = Game.sound.songVolume;
+                } else {
+                    Game.sound.songMuted = true;
+                    Game.sound.currentlyPlayingSong.volume = 0;
+                }
+            }
+        },
+        muteSFX: function () {
+            Game.sound.sfxMuted = !Game.sound.sfxMuted;
+        }
+    },
     setBackgroundColor: function (color) {
+        "use strict";
         this.stage.canvas.parentElement.style.background = color;
     },
     preloadAssets: function () {
+        "use strict";
         console.log("Preload Game Assets");
         
         function handleComplete() {
             Game.start();
         }
         
-        var manifest = [];
+        var manifest = [
+            {id: "gp_red", src: "graphics/GamePieces_Red.png"},
+            {id: "gp_blue", src: "graphics/GamePieces_Blue.png"},
+            {id: "gp_green", src: "graphics/GamePieces_Green.png"},
+            {id: "gp_purple", src: "graphics/GamePieces_Purple.png"},
+            {id: "gp_yellow", src: "graphics/GamePieces_Yellow.png"},
+            {id: "gp_glow", src: "graphics/GamePieces__Glow.png"},
+            {id: "gp_glowCover", src: "graphics/glowPieceCover.png"},
+            {id: "gp_outline", src: "graphics/GamePieces_Outline.png"},
+            {id: "gameSong", src: "sound/gameMusic_96kbps.mp3"},
+            {id: "changeMode", src: "sound/changeMode.mp3"},
+        ];
         
         if (manifest.length > 0) {
             Game.queue = new createjs.LoadQueue();
@@ -26,7 +90,11 @@ var Game = {
             handleComplete();
         }
     },
-    start: function () {
+    setupProgram: function () {
+        "use strict";
+        
+        createjs.Sound.registerPlugins([createjs.WebAudioPlugin, createjs.FlashPlugin]);
+        createjs.Sound.alternateExtensions = ["mp3"];
         
         Game.stage = new createjs.Stage("gameCanvas");
         
@@ -34,19 +102,37 @@ var Game = {
         createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCHED;
         createjs.Touch.enable(Game.stage, true, false);
         
-        Game.gamecontainer = new gdc.gameContainer(Game.stage.canvas.parentElement.width, Game.stage.canvas.parentElement.height);
+        Game.preloadAssets();
+    },
+    start: function () {
+        "use strict";
+        
+        gdc.gamePieceData.setupGamePieceImages()
+        
+        Game.gamecontainer = new gdc.gameContainer(Game.stage.canvas.width, Game.stage.canvas.height);
+        
         Game.stage.addChild(Game.gamecontainer);
         
-        Game.testHexagon = new createjs.Shape();
-        Game.testHexagon.x = Game.testHexagon.y = 100;
-        var hex = new hexagonMath.Hexagon(0, 0, 50, 30);
-        hex.draw(Game.testHexagon, "#330000", "red", 2);
+//        Game.sound.playSong("gameSong")
         
+        Game.testHexagon = new gdc.gamePiece("blue");
+        Game.testHexagon.x = Game.testHexagon.y = 100;
+        Game.testHexagon.changeTeam("purple");
+        Game.testHexagon.scaleX = 0.25;
+        Game.testHexagon.scaleY = 0.25;
+//        var hex = new hexagonMath.Hexagon(0, 0, 50, 30);
+//        hex.draw(Game.testHexagon, "#330000", "red", 2);
+//        
         Game.gamecontainer.gamefield.addChild(Game.testHexagon);
         
-        Game.stage.update();
         
-        
+        createjs.Tween.get(this).wait(2000).call(function () {
+            Game.sound.playSFX("changeMode", 1);
+            createjs.Tween.get(this).wait(200).call(function () {
+                Game.testHexagon.changeMode(2, 1900);
+            });
+            Game.stage.update();
+        }, undefined, this);
         console.log("Start Game");
         
         createjs.Ticker.on("tick", Game.tick);
@@ -54,9 +140,10 @@ var Game = {
         this.setBackgroundColor("black");
     },
     tick: function (event) {
+        "use strict";
         Game.runtime += event.delta;
         Game.stage.update();
         Game.testHexagon.y += 1;
     },
     testHexagon: undefined
-}
+};
--- src/gdc_gamePiece.js
+++ src/gdc_gamePiece.js
@@ -3,23 +3,120 @@ if (typeof gdc === "undefined") {
 }
 
 (function (scope) {
-    function gamePiece(HitAreaRadius) {
-        this.initialize(HitAreaRadius);
+    function gamePiece(Team, HitAreaRadius) {
+        this.initialize(Team, HitAreaRadius);
     }
     (function (obj) {
         var p = gamePiece.prototype = new createjs.Container();
+        
         p.Container_initialize = p.initialize;
-        p.initialize = function (HitAreaRadius) {
+        p.team = undefined;
+        p.mode = 0;
+        p.body = undefined;
+        p.body_glow = undefined;
+        p.change_glow = undefined;
+        
+        p.initialize = function (Team, HitAreaRadius) {
             this.Container_initialize();
             
             this.name = "gamePiece";
             this.setHitAreaCircle(HitAreaRadius);
+            
+            if (typeof Team !== "undefined") {
+                this.team = Team;
+            } else {
+                this.team = "red";
+            }
+            
+            this.setupImages();
         };
+        
+        p.setupImages = function () {
+            var b;
+            
+            b = {x: gdc.gamePieceData.gp_width, y: gdc.gamePieceData.gp_height};
+            
+            if (typeof this.body !== undefined) {
+                this.removeChild(this.body);
+            }
+            
+            this.body = new createjs.Sprite(gdc.gamePieceData.gp_SpriteSheets[this.team], this.mode);
+            this.body.regX = b.x / 2;
+            this.body.regY = b.y / 2;
+            this.addChild(this.body);
+            
+            if (typeof this.body_glow === "undefined") {
+                this.body_glow = new createjs.Sprite(gdc.gamePieceData.gp_Glow_SpriteSheet, this.mode);
+                this.body_glow.regX = b.x / 2;
+                this.body_glow.regY = b.y / 2;
+                this.body_glow.alpha = 0;
+                this.addChild(this.body_glow);
+            } else {
+                this.removeChild(this.body_glow);
+                this.addChild(this.body_glow);
+            }
+            
+            if (typeof this.change_glow === "undefined") {
+                b = {x: gdc.gamePieceData.gp_Glow_Overlay.width, y: gdc.gamePieceData.gp_Glow_Overlay.height};
+                
+                this.change_glow = new createjs.Bitmap(gdc.gamePieceData.gp_Glow_Overlay);
+                this.change_glow.regX = b.x / 2;
+                this.change_glow.regY = b.y / 2;
+                this.change_glow.alpha = 0;
+                this.addChild(this.change_glow);
+            } else {
+                this.removeChild(this.change_glow);
+                this.addChild(this.change_glow);
+            }
+        };
+        
         //WARING: VVV Does not work for hitTest() only works for mouseInteractions and getObjectsUnderPoint VVV
         p.setHitAreaCircle = function (Radius) {
             var circle = new createjs.Shape();
             circle.graphics.beginFill("red").drawCircle(0, 0, Radius);
             this.hitArea = circle;
+        };
+        
+        p.changeTeam = function (Color) {
+            if (typeof Color !== "undefined") {
+                this.team = Color;
+            } else {
+                this.team = "red";
+            }
+            this.setupImages();
+        };
+        
+        p.changeMode = function (NewMode, Duration) {
+            if (NewMode !== this.mode) {
+                if (typeof Duration === "undefined") {
+                    Duration = 0;
+                }
+                
+                this.mode = NewMode;
+                createjs.Tween.removeTweens(this.body_glow);
+                this.body_glow.alpha = 0;
+                createjs.Tween.removeTweens(this.change_glow);
+                this.change_glow.alpha = 0;
+
+                if (Duration === 0) {
+                    this.body.gotoAndStop(NewMode);
+                    this.body_glow.gotoAndStop(NewMode);
+                } else {
+                    var step2;
+                    
+                    step2 = function (Dur) {
+                        this.body.gotoAndStop(this.mode);
+                        this.body_glow.gotoAndStop(this.mode);
+                        createjs.Tween.get(this.body_glow).to({alpha: 0}, Dur / 2);
+                        createjs.Tween.removeTweens(this.change_glow);
+                        createjs.Tween.get(this.change_glow).to({alpha: 0}, Dur / 2);
+                    };
+                    
+                    createjs.Tween.get(this.body_glow).to({alpha: 1}, Duration / 2).call(step2, [Duration], this);
+                    createjs.Tween.get(this.change_glow).to({alpha: 0.5}, Duration / 2);
+                    
+                }
+            }
         };
         
         p.onTick = function (event) {
@@ -30,3 +127,47 @@ if (typeof gdc === "undefined") {
     scope.gamePiece = gamePiece;
 }(gdc));
 
+gdc.gamePieceData = {
+    totalStates: 3,
+    gp_width: 313,
+    gp_height: 318,
+    gp_clickRadius: 300,
+    resolutionMulti: 1,
+    gp_SpriteSheets: {},
+    gp_ColorNames: ["red", "blue", "green", "purple", "yellow"],
+    gp_Outline_SpriteSheet: undefined,
+    gp_Glow_SpriteSheet: undefined,
+    gp_Glow_Overlay: undefined,
+    setupGamePieceImages: function () {
+        //Setup the game piece images (as sprite sheets).
+        var data, cname, i;
+        for (i = 0; i < this.gp_ColorNames.length; i += 1) {
+            cname = this.gp_ColorNames[i];
+            data = {
+                framerate: 2,
+                images: [Game.queue.getResult("gp_" + cname)],
+                frames: {width: this.gp_width, height: this.gp_height}
+            };
+            this.gp_SpriteSheets[cname] = new createjs.SpriteSheet(data);
+        }
+        
+        //Setup the glow (white) gamepiece overlay image (as a sprite sheet).
+        data = {
+            framerate: 2,
+            images: [Game.queue.getResult("gp_glow")],
+            frames: {width: this.gp_width, height: this.gp_height}
+        };
+        this.gp_Glow_SpriteSheet = new createjs.SpriteSheet(data);
+        
+        //Setup the gamepiece outline image (as a sprite sheet).
+        data = {
+            framerate: 2,
+            images: [Game.queue.getResult("gp_outline")],
+            frames: {width: this.gp_width + 20, height: this.gp_height + 20} //The outline has 10px stroke.
+        };
+        this.gp_Outline_SpriteSheet = new createjs.SpriteSheet(data);
+        
+        //Retrieve glow overlay image
+        this.gp_Glow_Overlay = Game.queue.getResult("gp_glowCover");
+    }
+};
